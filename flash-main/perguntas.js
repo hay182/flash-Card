bbb95148431b6847e5fb5c85418acdc704da427e
hay182 <159156987+hay182@users.noreply.github.com>
2024-10-08 12:19:52 -0300

    08.10

---
+++ flash-main/perguntas.js
@@ -0,0 +1,26 @@
+criaCartao(
+    'Programação',
+    'O que é JavaScript?',
+    'O javascript éuma linguagem de programação'
+)
+criaCartao(
+    'Geografia',
+    'Qual a capital da frança?',
+    'A capital da frança é Paris'
+)
+criaCartao(
+    'Programação',
+    'O que é JavaScript?',
+    'O javascript éuma linguagem de programação'
+)
+criaCartao(
+    'Programação',
+    'O que é uma função',
+    'Uma função é um bloco de códegos que executauma função'
+)
+criaCartao(
+    'Matematicao',
+    'Qual é o resultado de 6 x 8',
+    'O resultado é 48'
+)
+    
