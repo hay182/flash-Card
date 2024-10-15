77667b6951982e29edfcf23e43fbfff5d5dca0d2
hay182 <159156987+hay182@users.noreply.github.com>
2024-10-15 12:32:56 -0300

    15.10

---
+++ flash-Card-main/perguntas.js
@@ -0,0 +1,21 @@
+criaCartao(
+    'Programação',
+    'O que é JavaScript?',
+    'O javascript éuma linguagem de programação'
+)
+criaCartao(
+    'Geografia',
+    'Qual a capital da frança?',
+    'A capital da frança é Paris'
+)
+criaCartao(
+    'Programação',
+    'O que é JavaScript?',
+    'O javascript éuma linguagem de programação'
+)
+criaCartao(
+    'Programação',
+    'O que é uma função',
+    'Uma função é um bloco de códegos que executauma função'
+)
+    
